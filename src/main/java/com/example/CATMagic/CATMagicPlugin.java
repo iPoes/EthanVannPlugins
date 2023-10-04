ad717759b714e0809b41d59c3104431ffaa88dd8
Skwabit <Bloodyalty001@gmail.com>
2023-10-04 19:33:04 +0200

    Ass runner

--- src/main/java/com/example/CATMagic/CATMagicPlugin.java
+++ src/main/java/com/example/CATMagic/CATMagicPlugin.java
@@ -1,6 +1,26 @@
 package com.example.CATMagic;
 
+import com.example.EthanApiPlugin.EthanApiPlugin;
+import com.example.PacketUtils.PacketUtilsPlugin;
+import com.google.inject.Provides;
+import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDependency;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@PluginDescriptor(
+        name = "AIO Magic",
+        description = "AIO Magic trainer",
+        enabledByDefault = false,
+        tags = {"ethan", "Poes", "iPoes", "Magic"}
+)
+@PluginDependency(PacketUtilsPlugin.class)
+@PluginDependency(EthanApiPlugin.class)
 
 public class CATMagicPlugin extends Plugin {
+
+    @Provides
+    public CATMagicConfig getConfig(ConfigManager configManager) {
+        return configManager.getConfig(CATMagicConfig.class);
+    }
 }
